b003951f54a6e5842d49878ba3dd8835309833c2
saksham kumar <sakshamkumarsingh005s@gmail.com>
2024-11-28 20:07:38 +0530

    THIS IS AN EMPLOYEE MANAGEMENT SYSTEM

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from './App'
+
+
+const App = () => {
+  return (
+   <> 
+    <Login/>
+   </>
+  )
+}
+
+
---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex items-centre justify-centre'>
+    <div className='border-2 border-red-600'>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Login
